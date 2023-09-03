cc478c715fd395bb9c2edad3da60cd10fb3413be
Guy Brown <guy.brown@justchecking.co.uk>
2023-09-03 17:35:27 +0100

    title case change

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -17,7 +17,7 @@ export function About() {
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-none">
           <p className="text-base font-semibold leading-7 text-salvify-primary">Work better</p>
-          <h2 className="font-display text-3xl tracking-tight text-salvify-secondary sm:text-4xl">An Insight into Salvify</h2>
+          <h2 className="font-display text-3xl tracking-tight text-salvify-secondary sm:text-4xl">An insight into Salvify</h2>
           <div className="mt-10 grid max-w-xl grid-cols-1 gap-8 text-base leading-7 text-gray-700 lg:max-w-none lg:grid-cols-2">
             <div>
               <p>
