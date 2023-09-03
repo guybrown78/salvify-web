fc6a150ba01f0a79392f718d6f796fc2a69c94ac
Guy Brown <guy.brown@justchecking.co.uk>
2023-09-03 15:13:43 +0100

    benefits

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,5 @@
 
-import clsx from 'clsx'
 
-import { Button } from '@/components/Button'
 import { Container } from '@/components/Container'
 
 
@@ -13,19 +11,37 @@ export function About() {
       className="bg-slate-50 py-20 sm:py-32"
     >
       <Container>
-        <div className="md:text-center">
-          <h2 className="font-display text-3xl tracking-tight text-salvify-secondary sm:text-4xl">
-            <span className="relative whitespace-nowrap">
-              <span className="relative">About Salvify</span>
-            </span>{' '}
-          </h2>
-          <p className="mt-4 text-lg text-salvify-secondary">
-            Lorum ipsum solar dolar...
-          </p>
+
+
+			<div className="py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-none">
+          <p className="text-base font-semibold leading-7 text-salvify-primary">Work better</p>
+          <h2 className="font-display text-3xl tracking-tight text-salvify-secondary sm:text-4xl">Our Story</h2>
+          <div className="mt-10 grid max-w-xl grid-cols-1 gap-8 text-base leading-7 text-gray-700 lg:max-w-none lg:grid-cols-2">
+            <div>
+              <p>
+								At Salvify, we are driven by a deep passion for healthcare excellence. Our mission is clear: to simplify the intricate world of medical inventory management. We know firsthand the challenges healthcare providers face – the need for rigorous compliance, the quest for cost-efficiency, and the unwavering commitment to patient care.
+              </p>
+              <p className="mt-8">
+								Our journey is marked by innovation, integrity, and an unwavering dedication to improving healthcare. We have crafted a cutting-edge SaaS application that empowers you to meet these challenges with ease.
+              </p>
+            </div>
+            <div>
+              <p>
+								Salvify stands as a beacon of change in healthcare inventory management. With a user-centric approach, we continually refine our software to adapt to your evolving needs.
+              </p>
+              <p className="mt-8">
+								Join us on this transformative healthcare journey. Discover how Salvify is shaping a future where medical inventory management is simple, compliant, and sustainable. Welcome to a new era in healthcare inventory management. Welcome to Salvify.
+              </p>
+            </div>
+          </div>
+
         </div>
-        <div className="-mx-4 mt-16 grid max-w-2xl grid-cols-1 gap-y-10 sm:mx-auto lg:-mx-8 lg:max-w-none lg:grid-cols-3 xl:mx-0 xl:gap-x-8">
-          
-        </div>
+      </div>
+    </div>
+
+
       </Container>
     </section>
   )
--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -13,7 +13,43 @@ export function Benefits() {
       className="bg-salvify-secondary py-20 sm:py-32"
     >
       <Container>
-        <div className="md:text-center">
+
+			{/* <div class="mx-auto max-w-7xl px-6 sm:mt-40 lg:px-8"> */}
+      <div class="mx-auto max-w-2xl lg:mx-0">
+        <h2 class="text-3xl font-bold tracking-tight text-slate-300 sm:text-4xl">Our values</h2>
+        <p class="mt-6 text-lg leading-8 text-slate-400">Salvify is more than just a medical inventory management solution, it is a transformative tool that revolutionises how healthcare providers operate. Our platform offers a multitude of advantages that enhance efficiency, compliance, and sustainability in the healthcare industry. Here are the top compelling benefits that make Salvify the go-to choice for healthcare inventory management:</p>
+      </div>
+      <dl class="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 text-base text-slate-400 leading-7 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+        <div className=''>
+          <dt class="font-semibold text-slate-300">Streamlined Inventory Control</dt>
+          <dd class="mt-1 "><span className='italic text-salvify-accent-light'>Maximize Efficiency</span> - Salvify simplifies medical inventory management, helping you optimize stock levels, reduce waste, and ensure crucial supplies are always at your fingertips.</dd>
+        </div>
+        <div>
+          <dt class="font-semibold text-slate-300">Unparalleled Compliance</dt>
+          <dd class="mt-1 "><span className='italic text-salvify-accent-light'>Stay Regulatory-Ready</span> - Our compliance tools and features empower you to effortlessly meet healthcare regulations and standards, minimizing compliance-related stress and risks.</dd>
+        </div>
+        <div>
+          <dt class="font-semibold text-slate-300">Cost Savings</dt>
+          <dd class="mt-1 "><span className='italic text-salvify-accent-light'>Boost Financial Health</span> - Our intelligent resource allocation and waste reduction capabilities save you money, enabling investments in critical areas of patient care and facility improvement.</dd>
+        </div>
+        <div>
+          <dt class="font-semibold text-slate-300">Real-time Visibility</dt>
+          <dd class="mt-1"><span className='italic text-salvify-accent-light'>Stay Informed</span> - Gain instant insights into your medical inventory, from anywhere. Our real-time tracking ensures you are always up-to-date on stock levels and supply chain performance.</dd>
+        </div>
+        <div>
+          <dt class="font-semibold text-slate-300">User-friendly Interface</dt>
+          <dd class="mt-1"><span className='italic text-salvify-accent-light'>Effortless Adoption</span> - Salvifys intuitive design makes onboarding and daily use a breeze, with minimal training required for your team to harness its full potential..</dd>
+        </div>
+        <div>
+          <dt class="font-semibold text-slate-300">Sustainability Champion</dt>
+          <dd class="mt-1">Reduce Environmental Impact<span className='italic text-salvify-accent-light'></span> - Salvify aids in minimizing medical waste, contributing to a more sustainable healthcare system and a greener planet.</dd>
+        </div>
+      </dl>
+    {/* </div> */}
+
+
+
+        {/* <div className="md:text-center">
           <h2 className="font-display text-3xl tracking-tight text-white sm:text-4xl">
             <span className="relative whitespace-nowrap">
               <span className="relative">The benefits of Salvify</span>
@@ -25,7 +61,7 @@ export function Benefits() {
         </div>
         <div className="-mx-4 mt-16 grid max-w-2xl grid-cols-1 gap-y-10 sm:mx-auto lg:-mx-8 lg:max-w-none lg:grid-cols-3 xl:mx-0 xl:gap-x-8">
           
-        </div>
+        </div> */}
       </Container>
     </section>
   )
