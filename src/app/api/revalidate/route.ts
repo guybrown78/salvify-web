27c87e96dfc18598dacb39d2a5e6211272f6b92b
Guy Brown <guy.brown@justchecking.co.uk>
2024-10-14 20:53:00 +0100

    POST

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,16 +1,28 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { revalidatePath } from 'next/cache';
 
-export async function GET(req: NextRequest) {
-  const secret = req.nextUrl.searchParams.get('secret');
-  const pathToRevalidate = req.nextUrl.searchParams.get('path');
-
+export async function POST(req: NextRequest) {
+  const { _type, secret } = await req.json();
+  
   if (secret !== process.env.SANITY_SECRET_TOKEN) {
     return NextResponse.json({ message: 'Invalid token' }, { status: 401 });
   }
 
   try {
-    if (typeof pathToRevalidate === 'string') {
+    let pathToRevalidate = '';
+
+    switch (_type) {
+      case 'faq':
+        pathToRevalidate = '/faq';
+        break;
+      case 'insightArticle':
+        pathToRevalidate = '/insights';
+        break;
+      default:
+        return NextResponse.json({ message: 'Unknown type' }, { status: 400 });
+    }
+
+    if (pathToRevalidate) {
       revalidatePath(pathToRevalidate);
       return NextResponse.json({ revalidated: true });
     } else {
