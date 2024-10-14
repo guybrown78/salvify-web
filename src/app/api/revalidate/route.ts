4b9ed90183632c34e1821d3cf432594ef8ea3d66
Guy Brown <guy.brown@justchecking.co.uk>
2024-10-14 20:11:54 +0100

    api/revalidate GET

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { revalidatePath } from 'next/cache';
 
-export async function POST(req: NextRequest) {
+export async function GET(req: NextRequest) {
   const secret = req.nextUrl.searchParams.get('secret');
   const pathToRevalidate = req.nextUrl.searchParams.get('path');
 
